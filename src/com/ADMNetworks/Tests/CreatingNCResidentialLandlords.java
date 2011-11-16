a4c06d966740a9a89ccb938006aad657f0704b55
Tapashwani <tapashwania@360logica.com>
2011-11-17 00:22:23 +0530

    Added 1 new test scripts

--- src/com/ADMNetworks/Tests/CreatingNCResidentialLandlords.java
+++ src/com/ADMNetworks/Tests/CreatingNCResidentialLandlords.java
@@ -27,6 +27,7 @@ public class CreatingNCResidentialLandlords {
 	baseUrl2 = readp.readApplicationFile("URLLabs");
         Object[][] retObjArr={{baseUrl1},
                             {baseUrl2}};
+        
         return(retObjArr);
     }
 	
@@ -51,6 +52,7 @@ public class CreatingNCResidentialLandlords {
 			Thread.sleep(7000);
 			Assert.assertTrue(selenium.isTextPresent("Get a quick quote"));
 			driver.findElement(By.linkText("Residential Landlords")).click();
+			custom.waitForElementPresentid(driver, "ctl00_MainContent_btnNext", 51);
 			Assert.assertTrue(custom.isElementPresent(driver, By.name("AgentRegistration")));
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("click here to sign in")));
 			Assert.assertTrue(selenium.isElementPresent("name=AgentRegistration"));
@@ -58,16 +60,20 @@ public class CreatingNCResidentialLandlords {
 			Assert.assertTrue(selenium.isTextPresent("Back"));		
 			Assert.assertTrue(custom.isElementPresent(driver,By.id("ctl00_MainContent_btnNext")));
 			driver.findElement(By.linkText("click here to sign in")).click();
-			driver.findElement(By.id("Username")).clear();
-			driver.findElement(By.id("Username")).sendKeys("360logica");
-			driver.findElement(By.id("Password")).clear();
-			driver.findElement(By.id("Password")).sendKeys("admtest");
+			custom.waitForElementPresentid(driver, "Username",61);
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("I forgot my username or password")));
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Return to My Homepage")));
-			driver.findElement(By.name("Login")).click();
+			custom.Login(driver, selenium, "Admin");
+			/*driver.findElement(By.id("Username")).clear();
+			driver.findElement(By.id("Username")).sendKeys("360logica");
+			driver.findElement(By.id("Password")).clear();
+			driver.findElement(By.id("Password")).sendKeys("admtest");*/
+			//driver.findElement(By.name("Login")).click();
 			Assert.assertTrue(custom.isElementPresent(driver, By.name("SignOut")));
 			Assert.assertTrue(selenium.isTextPresent("Search for existing client (optional)"));
+			custom.waitForElementPresentid(driver, "ctl00_MainContent_btnNext", 60);
 			driver.findElement(By.id("ctl00_MainContent_btnNext")).click();
+			Thread.sleep(3000);
 			Assert.assertTrue(custom.isElementPresent(driver, By.name("QuestionPage_Next")));
 			Assert.assertTrue(custom.isElementPresent(driver, By.name("SubmitQuote")));
 			Assert.assertTrue(selenium.isTextPresent("Statement of Facts"));
@@ -75,13 +81,15 @@ public class CreatingNCResidentialLandlords {
 			Assert.assertTrue(selenium.isTextPresent("Claims Details"));
 			Assert.assertTrue(selenium.isTextPresent("Optional Extras"));
 			Assert.assertTrue(selenium.isTextPresent("Client"));
+			custom.waitForElementPresent(driver, "#stub_page_Page2 > div.inner", 60);
 			driver.findElement(By.cssSelector("#stub_page_Page2 > div.inner")).click();
+			Thread.sleep(2000);
 			driver.findElement(By.id("Prop1AddressLine1")).clear();
 			driver.findElement(By.id("Prop1AddressLine1")).sendKeys("Test1");
 			driver.findElement(By.id("Prop1AddressLine2")).clear();
 			driver.findElement(By.id("Prop1AddressLine2")).sendKeys("test2");
 			driver.findElement(By.id("Prop1AddressTown")).clear();
-			driver.findElement(By.id("Prop1AddressTown")).sendKeys("Foster CIty");
+			driver.findElement(By.id("Prop1AddressTown")).sendKeys("Foster City");
 			driver.findElement(By.id("Prop1AddressCounty")).clear();
 			driver.findElement(By.id("Prop1AddressCounty")).sendKeys("USA");
 			driver.findElement(By.id("Prop1AddressPostCode")).clear();
@@ -98,9 +106,13 @@ public class CreatingNCResidentialLandlords {
 			driver.findElement(By.cssSelector("#stub_page_Page2 > div.inner")).click();
 			driver.findElement(By.id("Prop1YearBuilt")).clear();
 			driver.findElement(By.id("Prop1YearBuilt")).sendKeys("2010");
+			//custom.waitForElementPresent(driver, "#stub_page_Page5 > div.inner", 60);
 			driver.findElement(By.cssSelector("#stub_page_Page5 > div.inner")).click();
+			//custom.waitForElementPresent(driver, "#stub_page_Page6 > div.inner", 60);
 			driver.findElement(By.cssSelector("#stub_page_Page6 > div.inner")).click();
+			//custom.waitForElementPresent(driver, "#stub_page_Page1 > div.inner", 60);
 			driver.findElement(By.cssSelector("#stub_page_Page1 > div.inner")).click();
+			custom.waitForElementPresentid(driver, "ctl00_MainContent_CA1_AddressLine1", 60);
 			driver.findElement(By.id("ctl00_MainContent_CA1_AddressLine1")).clear();
 			driver.findElement(By.id("ctl00_MainContent_CA1_AddressLine1")).sendKeys("test line");
 			driver.findElement(By.id("ctl00_MainContent_CA1_TownCity")).clear();
@@ -109,16 +121,17 @@ public class CreatingNCResidentialLandlords {
 			driver.findElement(By.id("ctl00_MainContent_CA1_County")).sendKeys("USA");
 			driver.findElement(By.id("ctl00_MainContent_CA1_Postcode")).clear();
 			driver.findElement(By.id("ctl00_MainContent_CA1_Postcode")).sendKeys("sg2 8bj");
-			selenium.click("document.forms[0].elements[232]");
-			Assert.assertEquals("Please correct the problems on the pages with an exclamation mark before we proceed", selenium.getAlert());
+			//selenium.click("document.forms[0].elements[232]");
+			//Assert.assertEquals("Please correct the problems on the pages with an exclamation mark before we proceed", selenium.getAlert());
 			selenium.select("id=ctl00_MainContent_ClientContact_Title", "label=Mr");
 			driver.findElement(By.cssSelector("option[value=\"Mr\"]")).click();
-			Thread.sleep(5000);
+			custom.waitForElementPresentid(driver, "ctl00_MainContent_ClientContact_Firstname", 51);
 			driver.findElement(By.id("ctl00_MainContent_ClientContact_Firstname")).clear();
 			driver.findElement(By.id("ctl00_MainContent_ClientContact_Firstname")).sendKeys("testing");
 			driver.findElement(By.id("ctl00_MainContent_ClientContact_Surname")).clear();
 			driver.findElement(By.id("ctl00_MainContent_ClientContact_Surname")).sendKeys("Client");
-			driver.findElement(By.id("Next")).click();
+			selenium.click("document.forms[0].elements[232]");
+			custom.waitForElementPresentid(driver, "ctl00_MainContent_btnSave", 51);
 			Assert.assertTrue(selenium.isTextPresent("Quote submitted"));
 			String CaseID = driver.findElement(By.xpath("//div[@id='ctl00_MainContent_SummaryDefault']/h3/span")).getText();
 			System.out.println(CaseID);
@@ -126,7 +139,9 @@ public class CreatingNCResidentialLandlords {
 			Assert.assertTrue(custom.isElementPresent(driver, By.id("ctl00_MainContent_btnSave")));
 			Assert.assertTrue(custom.isElementPresent(driver, By.id("ctl00_MainContent_btnProceed")));
 			driver.findElement(By.id("ctl00_MainContent_btnSave")).click();
+			custom.waitForElementPresentid(driver, "Next", 51);
 			driver.findElement(By.id("Next")).click();
+			custom.waitForElementPresent(driver, "span.label", 51);
 			Assert.assertTrue(selenium.isTextPresent("Viewing Summary for " + CaseID));
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Endorsements")));
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Matrix")));
@@ -135,32 +150,75 @@ public class CreatingNCResidentialLandlords {
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Money")));
 			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Activity")));
 			Assert.assertTrue(selenium.isTextPresent("History"));
+			custom.waitForElementPresent(driver, "span.label", 61);
 			Assert.assertTrue(custom.isElementPresent(driver, By.cssSelector("span.label")));
-			Assert.assertTrue(selenium.isTextPresent("User Saved"));						
+			Assert.assertTrue(selenium.isTextPresent("User Saved"));	
+			driver.findElement(By.id("display_status")).click();
+			Thread.sleep(2000);
+			selenium.select("id=change_status", "label=Quotation");
+			//Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Change status only")));
+			//Assert.assertTrue(custom.isElementPresent(driver, By.cssSelector("#StatusChangeWarning > div > img")));
+			//Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Cancel")));
+			//driver.findElement(By.linkText("Change status only")).click();
+			custom.waitForElementPresentid(driver, "img[alt=\"Re-Quote\"]", 51);
+			Assert.assertTrue(custom.isElementPresent(driver, By.cssSelector("img[alt=\"Re-Quote\"]")));
+			Assert.assertTrue(custom.isElementPresent(driver, By.cssSelector("img[alt=\"ACME\"]")));
+			Assert.assertTrue(custom.isElementPresent(driver, By.cssSelector("img[alt=\"switch\"]")));
+			driver.findElement(By.linkText("Endorsements")).click();
+			Thread.sleep(3000);
+			Assert.assertTrue(selenium.isTextPresent("Conditions"));
+			driver.findElement(By.linkText("Add a new Conditions question")).click();
+			custom.waitForElementPresent(driver, "Question.Text", 60);
+			Assert.assertTrue(selenium.isTextPresent("Adding a Conditions item"));
+			driver.findElement(By.id("Question.Text")).clear();
+			driver.findElement(By.id("Question.Text")).sendKeys("Test Condition");
+			driver.findElement(By.id("Question.Save")).click();
+			Thread.sleep(3000);
+			driver.findElement(By.linkText("Add a new Memorandum question")).click();
+			driver.findElement(By.id("Question.Text")).clear();
+			driver.findElement(By.id("Question.Text")).sendKeys("Test Memo");
+			driver.findElement(By.id("Question.DefaultValue")).clear();
+			driver.findElement(By.id("Question.DefaultValue")).sendKeys("testing Memorandum.");
+			driver.findElement(By.id("Question.Save")).click();
+			Thread.sleep(3000);
+			driver.findElement(By.linkText("Matrix")).click();			
+			custom.waitForElementPresent(driver, "TotalPremiumNet_result_span", 60);
+			//String Totalpremium = driver.findElement(By.id("TotalPremiumNet_result_span")).getText();
+			//String TP = driver.findElement(By.id("TotalPremium_result_span")).getText();
+			driver.findElement(By.linkText("Documents")).click();
+			driver.findElement(By.cssSelector("img.hand")).click();
+			selenium.select("name=NewDocumentType", "label=Certificate - Combined Liability - Aviva");
+			driver.findElement(By.id("ctl00_ctl00_MainContent_Grey_PolicyContent_btnSave")).click();
+			driver.findElement(By.xpath("//div[@id='content']/table/tbody/tr/td/div[2]/div/div[9]/div/a")).click();
+			Thread.sleep(3000);
+			Assert.assertTrue(selenium.isTextPresent("Claims for this case"));
+			Assert.assertTrue(selenium.isTextPresent("Excesses for this case"));
+			
+			/*driver.findElement(By.linkText("Notes")).click();
+			driver.findElement(By.id("ctl00_ctl00_MainContent_Grey_PolicyContent_Img1")).click();
+			this.waitForElementPresent("body.cke_show_borders", 60);
+			Assert.assertTrue(selenium.isTextPresent("Add a note"));
+			Thread.sleep(3000);
+			Assert.assertTrue(custom.isElementPresent(driver, By.id("NewNote")));
+			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Cancel")));
+			driver.findElement(By.cssSelector("img[alt=\"close\"]")).click();*/
+			
+			driver.findElement(By.linkText("Money")).click();
+			Thread.sleep(5000);
+			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Take Card Payment")));
+			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Record Cheque")));
+			Assert.assertTrue(custom.isElementPresent(driver, By.linkText("Record Bank Payment")));
+			driver.findElement(By.linkText("Take Card Payment")).click();
+			custom.waitForElementPresent(driver, "img[alt=\"close\"]", 60);
+			driver.findElement(By.cssSelector("img[alt=\"close\"]")).click();
+			driver.findElement(By.linkText("Record Cheque")).click();
+			custom.waitForElementPresent(driver, "img[alt=\"close\"]", 60);
+			driver.findElement(By.cssSelector("img[alt=\"close\"]")).click();
+			driver.findElement(By.linkText("Record Bank Payment")).click();
+			custom.waitForElementPresent(driver, "img[alt=\"close\"]", 60);
+			driver.findElement(By.cssSelector("img[alt=\"close\"]")).click();
+			driver.findElement(By.linkText("Activity")).click();
+			driver.findElement(By.linkText("Logout")).click();
 	}
 	
-	public void waitForElementPresent(String locator, int timeout) throws Exception {
-		  for (int second = 0; second<200; second++) {
-		       if (second >= timeout)
-		           //throw new SeleniumException("Timout finding: " + locator);
-		           try {
-		                if (custom.isElementPresent(driver, By.cssSelector(locator)))
-		                    break;
-			            }
-			            catch (Exception ex) {}
-			            Thread.sleep(1000); 
-			       } 
-			 
-			}
-	public void waitForElementPresentid(String locator, int timeout) throws Exception {
-		  for (int second = 0; second<200; second++) {
-		       if (second >= timeout)
-		           try {
-		                if (custom.isElementPresent(driver, By.id(locator)))
-		                    break;
-			            }
-			            catch (Exception ex) {}
-			       } 
-			 
-			}
 }
